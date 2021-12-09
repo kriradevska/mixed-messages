d40acb67c17dad9fabab4c009537281be569f5cf
Kristina Radevska <kriradevska@gmail.com>
2021-12-09 21:40:55 +0100

    vegan soups added

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let meatSoup = ['Meatball Soup','Pork and Rice Noodle Soup','Italian Sausage and
 
 let vegetarianSoup = ['Simple Tomato Soup','Ribollita','Maroccan Lentil Quinoa Soup','Curried Zucchini Soup','Wild Mushroom and Sunchoke Soup','Potato Leek Soup','Creamy Mushroom Soup'];
 
+let veganSoup = ['Creamy Vegan Broccoli Soup','Curry Lentil Soup','Cauliflower Soup with Moroccan Spices','Vegan Corn Chowder','Tuscan Soup with White Beans','Kale Soup with Mushrooms and Garlic'];
