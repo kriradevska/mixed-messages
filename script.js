97dba1940d25ac610da6fec30c752cb1b0c402bb
Kristina Radevska <kriradevska@gmail.com>
2021-12-09 21:55:53 +0100

    randomization fn added

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+//arrays for the soups available
 let meatSoup = ['Meatball Soup','Pork and Rice Noodle Soup','Italian Sausage and Bean Soup','Slow-Cooked Mexican Beef Soup','Bratwurst Soup','Asian Vegetable -Beef Soup','Chicken Soup'];
 
 let vegetarianSoup = ['Simple Tomato Soup','Ribollita','Maroccan Lentil Quinoa Soup','Curried Zucchini Soup','Wild Mushroom and Sunchoke Soup','Potato Leek Soup','Creamy Mushroom Soup'];
 
-let veganSoup = ['Creamy Vegan Broccoli Soup','Curry Lentil Soup','Cauliflower Soup with Moroccan Spices','Vegan Corn Chowder','Tuscan Soup with White Beans','Kale Soup with Mushrooms and Garlic'];
+let veganSoup = ['Creamy Vegan Broccoli Soup','Curry Lentil Soup','Cauliflower Soup with Moroccan Spices','Vegan Corn Chowder','Tuscan Soup with White Beans','Kale Soup with Mushrooms and Garlic','Curried Carrot Soup'];
+
+//function for randomization
+let random = () => Math.floor(Math.random()*7);
+//console.log(random());
+
+
