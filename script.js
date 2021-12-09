f5989c4788523f17691b4807a5f4cb3a34fe4e00
Kristina Radevska <kriradevska@gmail.com>
2021-12-09 21:34:37 +0100

    meat and vegetarian soups array added

---
+++ script.js
@@ -0,0 +1,4 @@
+let meatSoup = ['Meatball Soup','Pork and Rice Noodle Soup','Italian Sausage and Bean Soup','Slow-Cooked Mexican Beef Soup','Bratwurst Soup','Asian Vegetable -Beef Soup','Chicken Soup'];
+
+let vegetarianSoup = ['Simple Tomato Soup','Ribollita','Maroccan Lentil Quinoa Soup','Curried Zucchini Soup','Wild Mushroom and Sunchoke Soup','Potato Leek Soup','Creamy Mushroom Soup'];
+
